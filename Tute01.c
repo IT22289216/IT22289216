0974418c5e2f4210182673f9315020fad25caad4
Nuwan Kodagoda <nuwank@gmail.com>
2021-07-11 14:32:06 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -1,4 +1,11 @@
-// Exercise 1 - Calculations
+/* Exercise 1 - Calculations
 
-// Write a C program to input marks of two subjects. Calculate and print the average of the two marks.
+   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+
+#include <stdio.h>
+
+int main() {
+  
+  return 0;
+}
 
